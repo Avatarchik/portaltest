f01eda059c50233d78d41ffc64a2ee5c2a07a047
Kain <kainsm1@jhuapl.edu>
2017-11-25 14:12:28 -0500

    the networking code is garbage

---
+++ Assets/Scripts/ShotEffectsManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class ShotEffectsManager : MonoBehaviour
+{
+    [SerializeField] ParticleSystem muzzleFlash;
+    [SerializeField] AudioSource gunAudio;
+    [SerializeField] GameObject impactPrefab;
+    [SerializeField] GameObject portalPrefab;
+
+    ParticleSystem impactEffect;
+
+    //Create the impact effect for our shots
+    public void Initialize()
+    {
+        impactEffect = Instantiate(impactPrefab).GetComponent<ParticleSystem>();
+    }
+
+    //Play muzzle flash and audio
+    public void PlayShotEffects()
+    {
+        muzzleFlash.Stop(true);
+        muzzleFlash.Play(true);
+        gunAudio.Stop();
+        gunAudio.Play();
+    }
+
+    //Play impact effect and target position
+    public void PlayImpactEffect(Vector3 impactPosition)
+    {
+        var portal = GameObject.Instantiate(portalPrefab);
+        portal.transform.position = impactPosition;
+        //impactEffect.transform.position = impactPosition;
+        //impactEffect.Stop();
+        //impactEffect.Play();
+    }
+}
