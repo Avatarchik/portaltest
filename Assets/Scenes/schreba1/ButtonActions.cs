29fdfb8d4c75f879f9dea4d6b46c716bb07aea3a
Blake Schreurs <blake.schreurs@jhuapl.edu>
2017-11-25 15:00:46 -0500

    Scene Updates

--- Assets/Scenes/schreba1/ButtonActions.cs
+++ Assets/Scenes/schreba1/ButtonActions.cs
@@ -7,15 +7,6 @@ public class ButtonActions : MonoBehaviour {
 
     public NetworkManager myNetworkmanager;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 
     public void JoinGame()
     {
