f67ee68e59008a5a8e2cc1370a99c2da04dffd8b
Blake Schreurs <blake.schreurs@jhuapl.edu>
2017-11-25 16:49:19 -0500

    VR Player In Scene

---
+++ Assets/Vr/Scripts/ButtonScripts/ButtonClick.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonClick : MonoBehaviour, IClickAction
+{
+    public void OnClick()
+    {
+        var go = gameObject.GetComponent<UnityEngine.UI.Button>();
+        if (go)
+        {
+            go.onClick.Invoke();
+        }
+    }
+}
---
+++ Assets/Vr/Scripts/VrAvatarController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class VrAvatarController : NetworkBehaviour
+{
+    public GameObject VrHeadset;
+
+    void Start()
+    {
+        var headset = GameObject.FindObjectOfType<SteamVR_Camera>();
+        VrHeadset = headset.gameObject;
+    }
+
+    void Update()
+    {
+        if (!isLocalPlayer) return;
+
+        transform.position = VrHeadset.transform.position;
+        transform.rotation = VrHeadset.transform.rotation;
+    }
+}
